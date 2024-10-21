674dfd1bfa4154f0380f75d740a868b3f5003fba
Sayid <sayidabyan@proton.me>
2024-10-22 06:43:15 +0700

    mbam2 #1

--- modules/common/base/default.nix
+++ modules/common/base/default.nix
@@ -18,11 +18,6 @@
   security.polkit.enable = true;
 
   networking.hostName = "nixos-${device}"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -80,18 +75,11 @@
   # Enable Flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.sayid = {
     isNormalUser = true;
     description = "sayid";
     extraGroups = [ "networkmanager" "wheel"];
-    # packages = with pkgs; [
-    #  firefox
-    #  thunderbird
-    # ];
   };
 
   services.flatpak.enable = true;
@@ -142,33 +130,30 @@
     programs.direnv.enable = true;
     programs.direnv.enableZshIntegration = true;
     programs.direnv.nix-direnv.enable = true; # prevent garbage collection
+    
+    # Custom Desktop Entry
+    xdg.desktopEntries = {
+      brave = {
+        name = "Brave Browser";
+        exec = "${pkgs.brave}/bin/brave --enable-features=TouchpadOverscrollHistoryNavigation";
+        startupNotify = true;
+        terminal = false;
+        icon = "brave-browser";
+        categories = ["Network" "WebBrowser"];
+        mimeType = ["application/pdf" "application/rdf+xml" "application/rss+xml" "application/xhtml+xml" "application/xhtml_xml" "application/xml" "image/gif" "image/jpeg" "image/png" "image/webp" "text/html" "text/xml" "x-scheme-handler/http" "x-scheme-handler/https"];
+      };
+    };
+    xdg.desktopEntries.brave.actions = {
+      "new-window" = {
+        name = "New Window";
+        exec = "${pkgs.brave}/bin/brave --enable-features=TouchpadOverscrollHistoryNavigation";
+      };
+      "new-private-window" = {
+        name = "New Private Window";
+        exec = "${pkgs.brave}/bin/brave --enable-features=TouchpadOverscrollHistoryNavigation --incognito";
+      };
+    };
   };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  # ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
