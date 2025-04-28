a682e6ca222ff95ebcea918ab91421c3818f4d81
sayidabyan <sayidabyan@proton.me>
2025-04-28 18:32:59 +0700

    28 Apr

--- modules/common/themes/default.nix
+++ modules/common/themes/default.nix
@@ -5,10 +5,10 @@
   fonts.fontDir.enable = true;
   fonts = {
     packages = with pkgs; [
-      #nerd-fonts.fira-code
-      #nerd-fonts.fira-mono
-      #nerd-fonts.symbols-only
-      nerdfonts
+      nerd-fonts.fira-code
+      nerd-fonts.fira-mono
+      nerd-fonts.symbols-only
+      # nerdfonts
       font-awesome 
       google-fonts 
       ipafont
