c7807e2980ad9709a05eca75a7d2b886932cbf28
sayid <sayidabyan@gmail.com>
2025-09-28 03:46:07 +0700

    28 Sep

--- modules/amdpc/base/default.nix
+++ modules/amdpc/base/default.nix
@@ -60,25 +60,5 @@
     # Bluetooth
     ATTRS{name}=="DualSense Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
   '';
-  hardware.enableAllFirmware = true;
-  services.ollama = {
-    enable = true;
-    package = pkgs.bleeding.ollama;
-    acceleration = "rocm";
-    host = "0.0.0.0";
-  };
-  services.nginx.virtualHosts."ollama.say.id" = {
-    locations."/" = {
-      proxyPass = "http://100.72.251.119:11434";
-      proxyWebsockets = true;
-      recommendedProxySettings = true;
-      extraConfig = ''
-        client_max_body_size 10000M;
-        proxy_read_timeout   600s;
-        proxy_send_timeout   600s;
-        send_timeout         600s;
-      '';
-    };
-  };
 }
 
